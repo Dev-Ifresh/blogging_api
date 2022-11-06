d0da7b903c4cc2eb6eb0093bec86624c92effb7a
Adun isaac <adunisaac@gmail.com>
2022-11-06 22:38:46 +0100

    Feat: contains the main logid

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require ("express");
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+
+const authRouter = require("../blog_api/routes/auth")
+const blogRouter = require("../blog_api/routes/blogs")
+
+
+require("dotenv").config()
+
+const app = express();
+
+app.use(passport.initialize());
+require("../blog_api/authentication/passport")
+
+
+
+require("../blog_api/db").connectToMongoDB()
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+const PORT = process.env.PORT
+
+app.get("/", (req,res)=>{
+    console.log("Hello server")
+    res.send("Welcome Server")
+})
+
+
+app.use("/", authRouter);
+app.use("/blogs", blogRouter);
+
+
+app.listen( PORT, () => {
+    console.log(`Welcome to http//:localhost:${PORT}`)
+});
+
+
